77ec3b987f5afacf537549c44d1812e6b37636e4
Benjamin Howell <ben.howell@utexas.edu>
2022-03-02 21:05:24 -0600

    re-generating models

--- lower_model.py
+++ lower_model.py
@@ -14,13 +14,16 @@ from sklearn.metrics import mean_squared_error
 # Lower: 20000
 # Luxury: 5000
 
-seats = ['lowerlevel.csv', 'luxurylevel.csv', 'middlelevel.csv', 'upperlevel.csv']
-
-df = pd.read_csv('data/lowerlevel.csv')
+df = pd.read_csv('data/LowerAttendance.csv')
 
 # df.rename(columns={c: c.strip() for c in df.columns.values.tolist()}, inplace=True) 
 def end_value(data, level):
+    data = data.reset_index()
+    data.columns = data.iloc[0]
+    data = data.drop(data.index[0])
     data = data.dropna()
+
+    data = data.astype({' Home Win %': 'float64', ' Visitor Win %': 'float64', ' Home Market Index': 'float64', ' Home Stars Index': 'int64', ' Visitor Stars Index': 'int64', ' Home AFP': 'int64', ' Visitor AFP': 'int64', ' Ticket Price': 'float64', ' Lower Section Attendance': 'int64'})
 
     if level == 'lower':
         n = 20000
@@ -31,17 +34,17 @@ def end_value(data, level):
     if level == 'luxury':
         n = 5000
 
-    data['pct'] = data['Attendance'] / n
+    data['pct'] = data[' Lower Section Attendance'] / n
     data['target'] = np.where(data['pct'] == 1, 15, np.where(data['pct'] == 0, -6.98, np.log(data['pct'] / (1 - data['pct']))))
 
-    model_df = data[['Home Win %', 'Visitor Win %', 'Home Market Index', 'Home Stars Index', 'Visitor Stars Index', 'Home AFP', 'Visitor AFP', 'Ticket Price', 'target']]
+    model_df = data[[' Home Win %', ' Visitor Win %', ' Home Market Index', ' Home Stars Index', ' Visitor Stars Index', ' Home AFP', ' Visitor AFP', ' Ticket Price', 'target']]
 
     return(model_df)
 
 model_df = end_value(data=df, level='lower')
-model_data = model_df[['Home Win %', 'Visitor Win %', 'Home Market Index', 'Home Stars Index', 'Visitor Stars Index', 'Home AFP', 'Visitor AFP', 'Ticket Price']]
+model_data = model_df[[' Home Win %', ' Visitor Win %', ' Home Market Index', ' Home Stars Index', ' Visitor Stars Index', ' Home AFP', ' Visitor AFP', ' Ticket Price']]
 model_y = model_df[['target']]
-x_train, x_test, y_train, y_test = train_test_split(model_data, model_y, test_size = 0.33, random_state=123)
+x_train, x_test, y_train, y_test = train_test_split(model_data, model_y, test_size = 0.01, random_state=123)
 dtrain = xgb.DMatrix(x_train, label=y_train)
 dtest = xgb.DMatrix(x_test, label=y_test)
 
